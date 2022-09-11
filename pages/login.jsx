114e3302bdf747cd6d1f21f95c1814ab75d61bff
HonvedoAdam <honvedoadamkj@gmail.com>
2022-09-11 14:54:47 +0200

    delete unncecesary content

---
+++ pages/login.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from '../styles/Login.module.css'
+
+const login = () => {
+    return (
+        <div>login</div>
+    )
+}
+
+export default login
---
+++ pages/registration.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from '../styles/Registration.module.css'
+
+const registration = () => {
+    return (
+        <div>registration</div>
+    )
+}
+
+export default registration
