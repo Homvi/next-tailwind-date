6af1d234dfe9556715e0f631f56247086e699593
HonvedoAdam <honvedoadamkj@gmail.com>
2022-09-13 19:45:38 +0200

    add teporary img to home

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
+import pimp from "../public/assets/img/pimp.png";
+import Image from "next/image";
+
 export default function Home() {
   return (
     <>
       <h1 className="text-3xl font-bold flex justify-center m-5 ">Főoldal</h1>
+      <Image
+        className="fixed bottom-0 left-0"
+        src={pimp}
+        alt="Picture of the author"
+        width={500}
+        height={400}
+        layout="responsive"
+      />
     </>
   );
 }
