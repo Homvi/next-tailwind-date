49384e3c255259a02769ce4e6dcfca5d73e5ac6b
HonvedoAdam <honvedoadamkj@gmail.com>
2022-09-15 19:00:35 +0200

    add privacy checkbox to registration

--- pages/registration.jsx
+++ pages/registration.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react'
 
 const registration = () => {
@@ -9,12 +10,20 @@ const registration = () => {
     return (
         <>
             <h1 className='text-3xl flex justify-center m-3 relative z-0'  >Regisztráció</h1>
-            <form action={handleSubmit} className="max-w-[500px] relative z-0 drop-shadow-sm bg-slate-100 p-5 mx-auto grid grid-rows-6 grid-flow-col gap-4 " >
+            <form action={handleSubmit} className="max-w-[700px] relative z-0 drop-shadow-sm bg-slate-100 p-5 flex flex-col mx-auto" >
                 <input type="text" placeholder='Vezetéknév' className='drop-shadow-md p-2 mt-1 mx-5' />
                 <input type="text" placeholder='Keresztnév' className='drop-shadow-md p-2 mt-1 mx-5' />
                 <input type="text" placeholder='felhasználónév' className='drop-shadow-md p-2 mt-1 mx-5' />
                 <input type="email" placeholder='e-mail' className='drop-shadow-md p-2 mt-1 mx-5' />
                 <input type="password" placeholder='Válassz jelszót' className='drop-shadow-md p-2 mt-1 mx-5' />
+                <div class="form-check flex justify-center  m-4 p-2 ">
+                    <label class="form-check-label inline-block text-gray-800" for="flexCheckDefault">
+                        Elolvastam és elfogadom az <span className='underline decoration-1' ><Link href="/termsofuse"  > Felhasználási feltételeket </Link></span> és az <span className='underline decoration-1' ><Link href="/privacypolicy">adatkezelési tájékoztató</Link></span> ban leírtakat.
+                    </label>
+                    <div className="check flex flex-col justify-end">
+                        <input className="form-check-input appearance-none h-5 w-5 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault" />
+                    </div>
+                </div>
                 <div class="flex space-x-2 justify-center">
                     <button onClick={handleSubmit} type="button" class="inline-block px-6 py-2.5 bg-gray-800 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-gray-700 hover:shadow-lg focus:bg-gray-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-600 active:shadow-lg transition duration-150 ease-in-out">Regisztrálok</button>
                 </div>
