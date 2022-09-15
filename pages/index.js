d1ebf0a95ad9bd427d78d3e0eeaac91b5522ca10
HonvedoAdam <honvedoadamkj@gmail.com>
2022-09-15 19:25:32 +0200

    change landing photo to next js

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,19 @@
-import pimp from "../public/assets/img/pimp.png";
+//import pimp from "../public/assets/img/pimp.png";
+import next from "../public/assets/img/next.svg";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <h1 className="text-3xl font-bold flex justify-center m-5 ">Főoldal</h1>
-      <Image
-        className="fixed bottom-0 left-0"
-        src={pimp}
-        alt="Picture of the author"
-        width={500}
-        height={400}
-        layout="responsive"
-      />
+      <div className="image-container m-5 mx-auto w-[300px] md:w-[400px] ">
+        <Image
+          className="h-3  w-screen"
+          src={next}
+          alt="next"
+          layout="responsive"
+        />
+      </div>
     </>
   );
 }
