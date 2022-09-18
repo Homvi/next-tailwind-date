b28a56afa822c7309c7f701675413dad9025e82c
HonvedoAdam <honvedoadamkj@gmail.com>
2022-09-18 11:39:19 +0200

    change input fields and add responsivity

--- pages/login.jsx
+++ pages/login.jsx
@@ -14,7 +14,7 @@ const login = () => {
 
     const handleEmail = (e) => {
         setEmail(e.target.value);
-        validateEmail("loginEmail")
+        validateEmail("emailInput")
     }
 
     const handlePassword = (e) => {
@@ -23,11 +23,21 @@ const login = () => {
 
     return (
         <>
-            <h1 className='text-3xl flex justify-center m-3 relative z-0'  >Bejelentkezés</h1>
-            <form action="#" className="max-w-[500px] relative bg-slate-100 p-5 z-0 drop-shadow-lg mx-auto grid grid-rows-3 grid-flow-col gap-4 " >
-                <input id='loginEmail' type="email" value={email} placeholder='E-mail' className='drop-shadow-md p-2 mt-1 mx-5' onBlur={e => handleEmail(e)} onChange={e => handleEmail(e)} />
-                <input type="password" value={password} autoComplete='true' placeholder='Jelszó' className='drop-shadow-md p-2 mt-1 mx-5' onChange={e => handlePassword(e)} />
-                <div className="flex space-x-2 justify-center">
+            <form action="#" className="max-w-[700px] relative z-0 drop-shadow-sm bg-slate-100 p-5 flex flex-col mx-auto xs:p-5 sm:mt-5" >
+                <h1 className='text-3xl flex justify-center m-3 relative z-0'  >Bejelentkezés</h1>
+                <div className="mb-2">
+                    <label htmlFor="emailInput" className="block mb-2 text-sm font-medium">E-mail</label>
+                    <input value={email} onChange={e => handleEmail(e)} type="email" id="emailInput" className=" text-sm rounded-lg  block w-full p-2.5" placeholder="example@example.com" />
+                    <p className="mt-1 text-sm text-red-600 dark:text-red-500 hidden">Kérlek adj meg egy valódi e-mail címet ahol elérhetünk!
+                    </p>
+                </div>
+                <div className="mb-2">
+                    <label htmlFor="passwordInput" className="block mb-2 text-sm font-medium">Jelszó</label>
+                    <input autoComplete='true' type="password" id="passwordInput" className=" text-sm rounded-lg  block w-full p-2.5" onChange={e => handlePassword(e)} placeholder="Jelszó" />
+                    <p className="mt-1 text-sm text-red-600 dark:text-red-500 hidden">A jelszónak legalább 5 karakter hosszúnak kell lennie és tartalmaznia kell nagybetűt és kisbetűt valamint számot.
+                    </p>
+                </div>
+                <div className="flex space-x-2 justify-center mt-5">
                     <button onClick={handleSubmit} type="button" className="inline-block px-6 py-2.5 bg-gray-800 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-gray-700 hover:shadow-lg focus:bg-gray-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-600 active:shadow-lg transition duration-150 ease-in-out">Bejelentkezés</button>
                 </div>
             </form>
--- pages/registration.jsx
+++ pages/registration.jsx
@@ -17,20 +17,52 @@ const registration = () => {
 
     return (
         <>
-            <h1 className='text-3xl flex justify-center m-3 relative z-0'  >Regisztráció</h1>
-            <form action="#" className="max-w-[700px] relative z-0 drop-shadow-sm bg-slate-100 p-5 flex flex-col mx-auto" >
+            <form action="#" className="max-w-[700px] relative z-0 drop-shadow-sm bg-slate-100 p-5 flex flex-col mx-auto xs:p-5 sm:mt-5 " >
+
+                <h1 className='text-3xl flex justify-center m-3 relative z-0'  >Regisztráció</h1>
+
+                {/*  Last name */}
                 <div className="mb-2">
                     <label htmlFor="lastNameInput" className="block mb-2 text-sm font-medium">Vezetéknév</label>
                     <input type="text" id="lastNameInput" className=" text-sm rounded-lg  block w-full p-2.5" placeholder="Vezetéknév" />
-                    <p className="mt-2 text-sm text-red-600 dark:text-red-500 hidden"><span className="font-medium">Oops!</span> A név mező kitöltése kötelező!
+                    <p className="mt-2 text-sm text-red-600 dark:text-red-500 hidden"><span className="font-medium">Hoppá!</span> A vezeténév mező kitöltése kötelező!
                     </p>
                 </div>
-                <input type="text" placeholder='Vezetéknév' className='drop-shadow-md p-2 mt-1 mx-5' />
-                <input type="text" placeholder='Keresztnév' className='drop-shadow-md p-2 mt-1 mx-5' />
-                <input type="text" placeholder='felhasználónév' className='drop-shadow-md p-2 mt-1 mx-5' />
-                <input type="email" id='registrationEmailInput' value={email} onChange={e => handleEmail(e)} placeholder='e-mail' className='drop-shadow-md p-2 mt-1 mx-5' />
-                <input autoComplete='true' type="password" placeholder='Válassz jelszót' className='drop-shadow-md p-2 mt-1 mx-5' />
-                <div className="form-check flex justify-center  m-4 p-2 ">
+
+                {/* First name */}
+                <div className="mb-2">
+                    <label htmlFor="FirstNameInput" className="block mb-2 text-sm font-medium">Keresztnév</label>
+                    <input type="text" id="FirstNameInput" className=" text-sm rounded-lg  block w-full p-2.5" placeholder="Keresznév" />
+                    <p className="mt-1 text-sm text-red-600 dark:text-red-500 hidden"><span className="font-medium">Hoppá!</span> A keresztnév megadása kötelező!
+                    </p>
+                </div>
+
+                {/* username */}
+                <div className="mb-2">
+                    <label htmlFor="userNameInput" className="block mb-2 text-sm font-medium">Felhasználónév</label>
+                    <input type="text" id="userNameInput" className=" text-sm rounded-lg  block w-full p-2.5" placeholder="Válassz felhasználónevet!" />
+                    <p className="mt-1 text-sm text-red-600 dark:text-red-500 hidden"><span className="font-medium">Hoppá!&nbsp;</span> Ez a felhasználónév már foglat!
+                    </p>
+                </div>
+
+                {/* email */}
+                <div className="mb-2">
+                    <label htmlFor="emailInput" className="block mb-2 text-sm font-medium">E-mail</label>
+                    <input value={email} onChange={e => handleEmail(e)} type="email" id="emailInput" className=" text-sm rounded-lg  block w-full p-2.5" placeholder="example@example.com" />
+                    <p className="mt-1 text-sm text-red-600 dark:text-red-500 hidden">Kérlek adj meg egy valódi e-mail címet ahol elérhetünk!
+                    </p>
+                </div>
+
+                {/* Password */}
+                <div className="mb-2">
+                    <label htmlFor="passwordInput" className="block mb-2 text-sm font-medium">Jelszó</label>
+                    <input autoComplete='true' type="password" id="passwordInput" className=" text-sm rounded-lg  block w-full p-2.5" placeholder="Jelszó" />
+                    <p className="mt-1 text-sm text-red-600 dark:text-red-500 hidden">A jelszónak legalább 5 karakter hosszúnak kell lennie és tartalmaznia kell nagybetűt és kisbetűt valamint számot.
+                    </p>
+                </div>
+
+                {/* Privacy policy */}
+                <div className="form-check flex justify-center  m-2 mb-5">
                     <label className="form-check-label inline-block text-gray-800" htmlFor="flexCheckDefault">
                         Elolvastam és elfogadom az <span className='underline decoration-1' ><Link href="/termsofuse"  > Felhasználási feltételeket </Link></span> és az <span className='underline decoration-1' ><Link href="/privacypolicy">adatkezelési tájékoztató</Link></span> ban leírtakat.
                     </label>
@@ -38,9 +70,12 @@ const registration = () => {
                         <input className="form-check-input appearance-none h-5 w-5 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault" />
                     </div>
                 </div>
+
+                {/* Submit Button */}
                 <div className="flex space-x-2 justify-center">
-                    <button onClick={handleSubmit} type="button" className="inline-block px-6 py-2.5 bg-gray-800 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-gray-700 hover:shadow-lg focus:bg-gray-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-600 active:shadow-lg transition duration-150 ease-in-out">Regisztrálok</button>
+                    <button onClick={handleSubmit} type="button" className="inline-block px-8 py-3 bg-gray-800 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-gray-700 hover:shadow-lg focus:bg-gray-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-600 active:shadow-lg transition duration-150 ease-in-out">Regisztrálok</button>
                 </div>
+
             </form>
         </>
     )
