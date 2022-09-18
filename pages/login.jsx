5273461d3b91584b9c92afb926b4196c752a379d
HonvedoAdam <honvedoadamkj@gmail.com>
2022-09-18 14:06:02 +0200

    show alert when missing info on login

--- pages/login.jsx
+++ pages/login.jsx
@@ -10,11 +10,29 @@ const login = () => {
     const handleSubmit = () => {
         console.log("The email is: ", email);
         console.log("The password is:  ", password);
+        const emailAlert = document.getElementById("emailAlertLogin")
+        const passwordAlert = document.getElementById("passwordAlertLogin")
+        const wrongPassOrEmailAlert = document.getElementById("wrongPassOrEmailAlert")
+        if (email.length < 3) {
+            emailAlert.classList.remove("hidden")
+        }
+        if (password.length < 4) {
+            passwordAlert.classList.remove("hidden")
+        }
+        if (password.length >= 4) {
+            passwordAlert.classList.add("hidden")
+        }
+        if (email.length >= 3) {
+            emailAlert.classList.add("hidden")
+        }
+        if (email.length >= 3 && password.length >= 4) {
+            wrongPassOrEmailAlert.classList.remove("hidden")
+        }
     }
 
     const handleEmail = (e) => {
         setEmail(e.target.value);
-        validateEmail("emailInput")
+
     }
 
     const handlePassword = (e) => {
@@ -28,15 +46,17 @@ const login = () => {
                 <div className="mb-2">
                     <label htmlFor="emailInput" className="block mb-2 text-sm font-medium">E-mail</label>
                     <input value={email} onChange={e => handleEmail(e)} type="email" id="emailInput" className=" text-sm rounded-lg  block w-full p-2.5" placeholder="example@example.com" />
-                    <p className="mt-1 text-sm text-red-600 dark:text-red-500 hidden">Kérlek adj meg egy valódi e-mail címet ahol elérhetünk!
+                    <p id="emailAlertLogin" className="mt-1 text-sm text-red-600 dark:text-red-500 hidden"> <span className="font-medium">Add meg az email címed a bejelentkezéshez</span>
                     </p>
                 </div>
                 <div className="mb-2">
                     <label htmlFor="passwordInput" className="block mb-2 text-sm font-medium">Jelszó</label>
                     <input autoComplete='true' type="password" id="passwordInput" className=" text-sm rounded-lg  block w-full p-2.5" onChange={e => handlePassword(e)} placeholder="Jelszó" />
-                    <p className="mt-1 text-sm text-red-600 dark:text-red-500 hidden">A jelszónak legalább 5 karakter hosszúnak kell lennie és tartalmaznia kell nagybetűt és kisbetűt valamint számot.
+                    <p id='passwordAlertLogin' className="mt-1 text-sm text-red-600 dark:text-red-500 hidden"> <span className="font-medium">Add meg az emailhez tartozó jelszót</span>
                     </p>
                 </div>
+                <p id='wrongPassOrEmailAlert' className="mt-1 text-sm text-red-600 dark:text-red-500 text-center hidden">Helytelen felhasználónév vagy jelszó
+                </p>
                 <div className="flex space-x-2 justify-center mt-5">
                     <button onClick={handleSubmit} type="button" className="inline-block px-6 py-2.5 bg-gray-800 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-gray-700 hover:shadow-lg focus:bg-gray-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-600 active:shadow-lg transition duration-150 ease-in-out">Bejelentkezés</button>
                 </div>
