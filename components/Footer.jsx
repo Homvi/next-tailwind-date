785bdf07ff826ef8967eb2009b5f5be7789b5109
HonvedoAdam <honvedoadamkj@gmail.com>
2022-09-21 20:27:38 +0200

    remove footer content

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,9 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <>
-            <div>Footer</div>
-            <span>Photo by <a href="https://unsplash.com/@evertonvila?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Everton Vila</a> on <a href="https://unsplash.com/s/photos/love?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-            </span>
+            <div></div>
         </>
     )
 }
