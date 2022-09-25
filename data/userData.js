301a2214e45c6e85e124a665206a86ec710c1b81
HonvedoAdam <honvedoadamkj@gmail.com>
2022-09-25 11:12:23 +0200

    remove fetch from index

---
+++ data/userData.js
@@ -0,0 +1,36 @@
+let users = {
+  users: [
+    {
+      id: 1,
+      lastName: "Joe",
+      firstName: "Doe",
+      age: 19,
+      email: "joedoe@example.com",
+      gender: true,
+    },
+    {
+      id: 2,
+      lastName: "Jane",
+      firstName: "Doe",
+      age: 23,
+      email: "janedoe@example.com",
+      gender: false,
+    },
+    {
+      id: 3,
+      lastName: "Nagy",
+      firstName: "Géza",
+      age: 40,
+      email: "nagygeza@example.com",
+      gender: true,
+    },
+    {
+      id: 4,
+      lastName: "Jakab",
+      firstName: "Judit",
+      age: 33,
+      email: "example@example.com",
+      gender: false,
+    },
+  ],
+};
