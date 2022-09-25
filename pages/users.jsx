6388c680e82c9c6a7616df810fc827ca67d90e71
HonvedoAdam <honvedoadamkj@gmail.com>
2022-09-25 19:00:15 +0200

    add responsivitiy grid to users

--- pages/users.jsx
+++ pages/users.jsx
@@ -3,7 +3,7 @@ import User from '../components/User';
 
 export const getServerSideProps = async () => {
     const res = await fetch("https://next-date.netlify.app/api/hello");
-   // const res = await fetch("http://localhost:3000/api/hello");
+    // const res = await fetch("http://localhost:3000/api/hello");
     let data = await res.json();
 
     return {
@@ -14,8 +14,8 @@ export const getServerSideProps = async () => {
 const users = ({ users }) => {
     return (
         <>
-            <h1 className="text-center text-xl p-5">Felhasználók</h1>
-            <div className="p-2">
+            <h1 className="text-center text-xl p-5">Regisztrált felhasználók</h1>
+            <div className="p-3  grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-3 max-w-[1240px] mx-auto">
                 {users && users.map((user, id) => {
                     return (
                         <>
