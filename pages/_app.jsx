9db8f1e738590947c14dd2deaa1283feb5145483
HonvedoAdam <honvedoadamkj@gmail.com>
2022-10-01 21:01:23 +0200

    changefavicon

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -9,6 +9,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>DateNow</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name='description' content='Találd meg a párod akár perceken belül' />ó
       </Head>
       <Navbar />
       <Component {...pageProps} />
